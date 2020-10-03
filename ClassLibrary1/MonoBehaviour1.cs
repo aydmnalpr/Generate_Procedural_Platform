9b1e4e7f5fd095be6c5dcdb5a0be8f9de0650bb7
Alper Ayduman <aydumanalper@gmail.com>
2020-10-03 15:51:05 +0300

    Initial commit

---
+++ ClassLibrary1/MonoBehaviour1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+using System.Collections.Generic;
+using Random = UnityEngine.Random;
+
+namespace ClassLibrary1
+{
+    
+    public class GenerateWold : MonoBehaviour
+    {
+        public GameObject[] platforms;
+
+        private void Start()
+        {
+            Vector3 pos = new Vector3(0,0,0);
+            for (int i = 0;i < 20;i++)
+            {
+                int platformNumber = Random.Range(0, platforms.Length);
+                Instantiate(platforms[platformNumber], pos, Quaternion.identity);
+                pos.z += 10; //interval z position
+            }
+        }
+
+        
+    }
+}
